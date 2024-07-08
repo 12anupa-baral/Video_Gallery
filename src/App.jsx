acc29a49cc0fdb6c1caba0493890a33f8770e58a
Anupa Baral <baralanupa988@gmail.com>
2024-07-08 14:55:23 +0545

    Header created

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+import Header from './components/Header'
+
+function App() {
+  return (
+    <>
+    <Header title="React Videos" subtitle="A brief history of React"/>
+
+    </>
+  )
+}
+
+export default App
