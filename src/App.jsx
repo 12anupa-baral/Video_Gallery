b9dc022eba4160fc092116d5e60dc713e4245447
Anupa Baral <86550755+12anupa-baral@users.noreply.github.com>
2024-07-09 17:34:48 +0545

    videolist deleted

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 import Header from './components/Header'
-import VideoList from './components/VideoList'
 import Apifetch from './components/Apifetch'
-// import Axios from './components/UsingAxios'
 
 function App() {
   return (
